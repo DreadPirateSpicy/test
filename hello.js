fe68558aed7f112a8a872510a1de70cdebfa589d
DreadPirateSpicy <165183548+DreadPirateSpicy@users.noreply.github.com>
2024-03-27 18:13:44 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-// the hello world program
-console.log('Hello World');
+// Select the <div> element with id "message"
+const messageElement = document.getElementById('message');
+
+// Set the innerHTML of the <div> element to "Hello World"
+messageElement.innerHTML = 'Hello World!';
